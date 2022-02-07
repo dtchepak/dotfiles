5390cdf265a8cd49703835154301dd3475cf47ce
David Tchepak <dave@davesquared.net>
2022-02-08 10:24:17 +1100

    add dodgy install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Run from ~
+# requires dotfiles
+ln -s ~/dev/git/dotfiles/tmux.conf .tmux.conf 
+ln -s ~/dev/git/dotfiles/zshenv .zshenv
+ln -s ~/dev/git/dotfiles/zshrc .zshrc
+ln -s ~/dev/git/dotfiles/ghci .ghci
+ln -s ~/dev/git/dotfiles/.gitconfig .gitconfig
+ln -s ~/dev/git/dotfiles/.gitignore_global .gitignore_global
+# requires vimfiles
+ln -s ~/dev/git/vimfiles/ .vim
+# Requires SSH
+ln -s ~/SSH .ssh 
